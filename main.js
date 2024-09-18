6e11a68b060c0b1986ac6bdf341988fcd49c7f88
kietduong09 <kietduong220981@gmail.com>
2024-09-18 08:51:35 +0700

    virtual calculator

---
+++ main.js
@@ -0,0 +1,21 @@
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function deleteLast() {
+    let currentValue = document.getElementById('display').value;
+    document.getElementById('display').value = currentValue.slice(0, -1);
+}
+
+function calculateResult() {
+    let expression = document.getElementById('display').value;
+    try {
+        document.getElementById('display').value = eval(expression);
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
